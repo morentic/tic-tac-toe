33d3763774e373300eb7e2198bea5570cc3dda5d
morentic <mkopp4191@gmail.com>
2022-07-10 15:55:08 +0200

    fix wins

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -4,8 +4,8 @@ import { StrictMode, useState, useEffect } from "react";
 function App() {
   const wins = [
     [1, 2, 3],
-    [3, 4, 5],
-    [6, 7, 9],
+    [4, 5, 6],
+    [7, 8, 9],
     [1, 4, 7],
     [2, 5, 8],
     [3, 6, 9],
